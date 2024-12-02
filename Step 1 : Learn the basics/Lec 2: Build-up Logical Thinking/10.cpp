efb56088b183085eb29357c0c1f92a425959f7a2
aryan2729 <aryancyber282h@gmail.com>
2024-12-02 23:32:39 +0530

    adding patter code

---
+++ Step 1 : Learn the basics/Lec 2: Build-up Logical Thinking/10.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+// M.I🚀. -> if u're using (i=0) then take (i<n) and if u're using (i = 1) then use (i<=n )
+
+// Pattern 10
+
+void print10(int n ){
+
+    
+
+
+  
+}
+
+
+int main (){
+
+    int t;
+    cout<<"Write how much input u wanna given "<<endl;
+    cin>> t;
+    for (int i = 0; i <t ; i++){
+        int n ;
+        cin>>n;
+        print10(n);
+    }
+
+
+    return 0;
+}
