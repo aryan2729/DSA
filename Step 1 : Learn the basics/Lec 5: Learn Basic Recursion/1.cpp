a13fe47cb0a346b401136faaf263aab3d9c628fc
aryan2729 <aryancyber282h@gmail.com>
2024-12-12 22:45:54 +0530

    Recursion code

--- Step 1 : Learn the basics/Lec 5: Learn Basic Recursion/1.cpp
+++ Step 1 : Learn the basics/Lec 5: Learn Basic Recursion/1.cpp
@@ -4,7 +4,28 @@ using namespace std;
 /*🔥(M.I.) Recursion -> When a function call itself until 
  a specific condition met. */
 
+// Most of part like printing N things , N-1 , 1 -N covered in notes (copy)
+
+
+//🚀 Sum of first N no.s 
+int  sum(int n){            //func
+
+    if(n== 0) return 0;
+
+    return n + sum(n-1) ;
+}
+
 int main (){
-                // No code  cuz everything written in notes copy & not provided extra Qsn .
+
+    cout<<"Write n value for sum of N no.s"<<endl;
+    int n ;
+    cin>>n;
+
+    cout<<(sum(n)) ;            // findng Sum of n no.s
+
+
+
+
+
     return 0;
 }
