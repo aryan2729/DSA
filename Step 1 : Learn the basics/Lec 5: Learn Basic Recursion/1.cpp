b3745c9cb3c3baaa2134e49037b3b6dbce7850e0
aryan2729 <aryancyber282h@gmail.com>
2024-12-13 21:27:57 +0530

    Reverse Array

--- Step 1 : Learn the basics/Lec 5: Learn Basic Recursion/1.cpp
+++ Step 1 : Learn the basics/Lec 5: Learn Basic Recursion/1.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 /*🔥(M.I.) Recursion -> When a function call itself until 
@@ -15,14 +16,43 @@ int  sum(int n){            //func
     return n + sum(n-1) ;
 }
 
+
+// For factorial of N no.s 
+
+int fact( int B){
+    if(B==0) return 1;
+
+    return B * fact(B-1) ;
+}
+
+//🚀 Reverse an array(2nd way )
+void reverseArr2(int i , int arr[] , int m ){
+
+    if( i >= m/2) return ;
+
+    swap(arr[i], arr[m-i-1]); 
+
+    reverseArr2(i+1,arr,m);
+}
+
+
 int main (){
 
-    cout<<"Write n value for sum of N no.s"<<endl;
-    int n ;
-    cin>>n;
+    // cout<<"Write n value for sum of N no.s";
+    // int n ;
+    // cin>>n;
 
-    cout<<(sum(n)) ;            // findng Sum of n no.s
+    // cout<<(sum(n)) ;            // findng Sum of n no.s
 
+
+        int m;
+        cin >> m ;        
+        int arr[m];
+        for(int i = 0; i<m ; i++) cin>> arr[i];   // input array that u want to reverse 
+
+        reverseArr2(0,arr,m);
+
+        for(int i = 0 ; i< m ; i++) cout<<arr[i]<<" ";  // printing swaped array or reverse array 
 
 
 
